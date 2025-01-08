d05da938e4e640672a6af56a2d1409647cdcdfa2
Feyyaz <thefeyzu@gmail.com>
2025-01-08 20:48:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 

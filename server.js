83e4f9a6bf61f826c9220db3758526803dee7f17
Feyyaz <thefeyzu@gmail.com>
2025-01-09 22:29:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
+const socket = io();
+
 
 // Sunucu ayarları
 app.use(express.static('public'));
